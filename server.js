18a98d5a2362715dc1395ff50ce15a106d31268f
Cleytonf08 <cleytonf@hotmail.com>
2026-04-24 16:27:58 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-// rota básica
+// servir arquivos da pasta public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// rota principal (garante que abre index.html)
 app.get('/', (req, res) => {
-  res.send("Site rodando 🚀");
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// porta correta pro Render
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {

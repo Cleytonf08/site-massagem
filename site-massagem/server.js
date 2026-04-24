455f247720824c6a87a53254443651c743d569f6
Cleytonf08 <cleytonf@hotmail.com>
2026-04-24 14:25:13 -0300

    Update server.js

--- site-massagem/server.js
+++ site-massagem/server.js
@@ -1,7 +1,11 @@
+const express = require('express');
+const app = express();
+
+// mostrar pasta public
+app.use(express.static('public'));
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Rodando"));
-app.get('/agendamentos', (req, res) => {
-  db.all("SELECT * FROM agendamentos", [], (err, rows) => {
-    res.json(rows);
-  });
+
+app.listen(PORT, () => {
+  console.log("Rodando");
 });

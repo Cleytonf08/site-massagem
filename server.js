9d9bac6676ae6960cc600489d00db42491a4f82e
Cleytonf08 <cleytonf@hotmail.com>
2026-04-24 14:08:06 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+const express = require('express');
+const app = express();
+
+// rota básica
+app.get('/', (req, res) => {
+  res.send("Site rodando 🚀");
+});
+
+// porta correta pro Render
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Rodando"));
-app.get('/agendamentos', (req, res) => {
-  db.all("SELECT * FROM agendamentos", [], (err, rows) => {
-    res.json(rows);
-  });
+
+app.listen(PORT, () => {
+  console.log("Rodando");
 });
